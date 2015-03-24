68acad8d2aaf53a8b64cf48f5cef150ebcd0d234
imixtron <imaadabbasi@hotmail.com>
2015-03-25 02:12:03 +0500

    sorting working

--- assets/js/FormBuilder.js
+++ assets/js/FormBuilder.js
@@ -1,3 +1,4 @@
+// "use strict";
 /*  FormBuilder.js    */
 /*  Author: imixtron  */
 
@@ -9,7 +10,7 @@ dragIcon.src = 'assets/img/icons/appbar.layer.add.png';
 dragIcon.width = 100;
 pprlsrowCount = 0;
 formID = "dummy";
-controlProperties = {};
+cP = {};
 divId = "DropZone";
 
 // Drag N Drop Events:
@@ -28,14 +29,16 @@ checkTouchClasses = function(node){
 getParentNode = function(node,chkClasses){
 
 	var newItem = node;
-	if (newItem.dataset.nodeType=="control")
-		newItem = newItem.parentNode;
+
 	newItem = newItem.parentNode;
 
-	if (true)
-		if (checkTouchClasses($(node)))
+	if (chkClasses==true){
+		if (checkTouchClasses($(node))){
+			if (newItem.dataset.nodeType=="control")
+				newItem = newItem.parentNode;}
 			return newItem;
-	else
+	}
+	else if (chkClasses==false)
 		return newItem;
 
 }
@@ -50,7 +53,6 @@ touchInit = function(){
 
 		item.addEventListener("touchstart", handleStart = function(ev){
 			console.log("Tstart");
-			console.log(ev.target);
 
 			draggable = document.getElementById(ev.target.id);
 			if (draggable==null)
@@ -71,9 +73,7 @@ touchInit = function(){
 
 			if(touch_checkDropzone(ex,vy)==true){
 				var nodeCopy = draggable.cloneNode(true);
-				// console.log(nodeCopy);
 				$(nodeCopy).data('uId', generateId());
-				console.log($(nodeCopy).data('uId'));
 
 				InsertIntoForm(nodeCopy);
 				draggable = null;
@@ -92,7 +92,7 @@ touchInit = function(){
 		}, false);
 		
 		item.addEventListener("touchmove", handleMove = function(ev){
-			// console.log("Tmove");
+			console.log("Tmove");
 			touch = event.targetTouches[0];
 
 			ex = touch.pageX;
@@ -145,7 +145,6 @@ CRdragStart = function (ev) {
 
 CRdragEnter = function(ev){
 	console.log("EV->Enter:");
-	console.log(ev.dataTransfer.getData("Text/html"));
 
 	hover_DropZone(ev.target.id,1);
 
@@ -171,13 +170,9 @@ CRdragDrop = function(ev) {
 	console.log("EV->Drop: ")
 	hover_DropZone(ev.target.id,0);
 
-	var data = ev.dataTransfer.getData("Text/html");
-	var nodeCopy = document.getElementById(data).cloneNode(true);
-	console.log("data");
-	console.log(data);
+	var nodeID = ev.dataTransfer.getData("Text/html");
+	var nodeCopy = document.getElementById(nodeID).cloneNode(true);
 	// var uId  = generateId(data);
-	$(nodeCopy).data('uId', generateId());
-	console.log($(nodeCopy).data('uId'));
 
 	InsertIntoForm(nodeCopy);
 
@@ -186,8 +181,10 @@ CRdragDrop = function(ev) {
 }
 
 InsertIntoForm = function(nodeCopy){
+	$(nodeCopy).data('uId', generateId());
  	setProperties(nodeCopy);
-	var container = fetchRow($("#pprlsForm"))
+
+	var container = document.getElementById('pprlsForm');
 	container.appendChild(nodeCopy);
 }
 
@@ -196,57 +193,48 @@ InsertIntoForm = function(nodeCopy){
 
 // Other Functions:
 setProperties = function(nodeCopy){
-	var data = nodeCopy.id;
-	uId = $(nodeCopy).data('uId')
-	console.log(nodeCopy);
+	var nodeID = nodeCopy.id;
+	uId = $(nodeCopy).data('uId');
 
-    if (controlProperties[uId]==null) //Init for the new control
+    if (cP[uId]==null) //Init for the new control
     {
 	    //Adding our controls properties to the object
-	    controlProperties[uId] = {
+	    cP[uId] = {
 	    	_uid : uId,
-	    	type : data,
+	    	type : nodeID,
+	    	required: false,
+	    	singleItem: false,
 	    	dataType: null,
 	    	label : null,
 	    	placeholder : null,
 	    	cssClass : "form-group",
-	    	width : "col-xs-6",
-	    	required: false,
-	    	values :[{
-	    		name: uId,
-	    		value: "value 1"
-	    	}]
+	    	width : 6,
+	    	values :[undefined]
 	    }
 	    popProperties(nodeCopy,uId);
-	    return remDataAttribs(nodeCopy,uId);
+	    remDataAttribs(nodeCopy,uId);
+	    return true;
 	}
 
 	popProperties(nodeCopy,uId);
-	return remDataAttribs(nodeCopy,uId);
+	remDataAttribs(nodeCopy,uId);
+    return true;
 
 }
 
 popProperties = function(nodeCopy, uId){
 	$("#modal-properties").modal("show");
-	console.log(controlProperties[uId]);
 
-	var pObj = controlProperties[uId];
+	var cP_Obj = cP[uId];
 
-	$("#_uId").html(pObj._uid);
-
-	$("#_type").html(analyzeProperties.type(pObj._uid));
-	
-	$("#_dataType").html(analyzeProperties.dataType(pObj.type,pObj.dataType));
-
-	$("#_label").html(analyzeProperties.label(pObj.type,pObj.label));
-
-	$("#_placeholder").html(analyzeProperties.placeholder(pObj.type,pObj.placeholder));
-	// analyzeProperties.width(pObj.type,pObj.width)
-
-	$("#_value").html(analyzeProperties.value(pObj.type,pObj.values.length,pObj._uid));
-
-	// $("#modal-properties #pprlsProperties").html(tempHtml);
-	// $("#modal-properties table").html(tblInner);
+	analyzeProperties.uid(cP_Obj._uid);
+	analyzeProperties.type(cP_Obj.type);
+	analyzeProperties.required(cP_Obj.required);
+	analyzeProperties.dataType(cP_Obj.type);
+	analyzeProperties.label(cP_Obj.type,cP_Obj.label);
+	analyzeProperties.placeholder(cP_Obj.type,cP_Obj.placeholder);
+	analyzeProperties.width(cP_Obj.type,cP_Obj.width);
+	analyzeProperties.value(cP_Obj.type,cP_Obj._uid);
 	
 }
 
@@ -263,10 +251,13 @@ remDataAttribs = function(nodeCopy, uId){
 	nc.removeAttr('left');
 	nc.removeAttr('z-index');
 
-	nc.addClass('form-group col-xs-6');
+	nc.addClass('col-xs-'+cP[uId].width);
+	nc.addClass('form-group');
+	nc.addClass('ui-state-default');
 	nc.attr("id", uId);
 	nc.attr('onclick', 'setProperties(this)');
 
+	$("#_closeModal").attr('onclick', "saveControl('"+uId+"')");
 	$("#_delControl").attr('onclick', "deleteControl('"+uId+"')");
 	$("#_svControl").attr('onclick', "saveControl('"+uId+"')");
 
@@ -274,85 +265,160 @@ remDataAttribs = function(nodeCopy, uId){
 }
 
 analyzeProperties = {
-	type : function(uId){
-		return controlProperties[uId].type;
+	uid : function(uId){
+		setString("_uId",uId);
 	},
-	dataType : function(type,dataType){
-		var temp;
+	type : function(type){
+		setString("_type",type);
+	},
+	required : function(req){
+		var temp = document.getElementById('_required');
+		temp.childNodes[0].checked = req;
+	},
+	dataType : function(type){
+		var temp = document.getElementById('_dataType');
+		var inr;
 		switch(type){
 			case "TextBoxRight":
 			case "TextBoxLeft":
-					temp = "<select class='form-control'>"+
-								"<option>Select one</option>"+
+					inr = "<select class='form-control'>"+
 								"<option value='text'>Text</option>"+
 								"<option value='email'>Email</option>"+
 								"<option value='number'>Number</option>"+
 								"<option value='date'>Date</option>"+
 						   "</select>";
 						   break;
+			case "Radio":
+			case "Dropdown":
 			case "TextArea":
 			case "CheckBox":
 			case "Paragraph":
-					temp = "<select class='form-control' disabled='disabled'>"+
+					inr = "<select class='form-control' disabled='disabled'>"+
 							"<option>Default</option>"+
 						   "</select>";
 		}
-		return temp;
+		temp.innerHTML = inr;
 	},
 	label : function(type,label){
-		if (label==null)
-			label = "label";
-		return "<input id='pprlPropLabel' class='form-control' value='"+label+"'/>";
-	},
-	placeholder : function(type,placeholder){
-		if (placeholder==null)
-			placeholder = "placeholder";
-		return "<input id='pprlPropPlaceholder' class='form-control' value='"+placeholder+"'/>";
-	},
-	width : function(type,width){
-		temp = "<select class='form-control'>"+
-					"<option>Select one</option>"+
-					"<option value='0.25'>Quarter</option>"+
-					"<option value='0.5'>Half</option>"+
-					"<option value='1'>Full</option>"+
-			   "</select>";
-		return temp;
-	},
-	value: function(type,length,uId){
-		console.log(type);
+		var temp = document.getElementById('_label');
 		switch(type){
-			case "Paragraph":
-				setString("_valueName","Text");
-				checkVal(uId);
-				break;
 			case "TextBoxRight":
 			case "TextBoxLeft":
 			case "TextArea":
-				setString("_valueName","Default Value:");
-				checkVal(uId);
+			case "CheckBox":
+				if (label==null)
+					label = "label";
+				temp.querySelector("input").value = label;
+			   break;
+			case "Paragraph":
+				temp.querySelector("input").disabled = true;
+				temp.querySelector("input").value = null;
+				break;
+		}
+	},
+	placeholder : function(type,placeholder){
+		var temp = document.getElementById('_placeholder');
+		switch(type){
+			case "TextBoxRight":
+			case "TextBoxLeft":
+			case "TextArea":
+			case "CheckBox":
+				if (placeholder==null)
+					placeholder = "placeholder";
+				temp.querySelector("input").value = placeholder;
+
+			   break;
+			case "Paragraph":
+				temp.querySelector("input").value = null;
+				temp.querySelector("input").disabled = true;
+				break;
+		}
+	},
+	width : function(type){
+		var temp = document.getElementById('_width');
+		switch(type){
+			case "TextArea":
+			case "Paragraph":
+				$("#_width select")[0].selectedIndex = 1;
+				$("#_width select")[0].disabled = true;
+				break;
+			case "RadioButton":
+			case "CheckBox":
+				$("#_width select")[0].selectedIndex = 0;
+				$("#_width select")[0].disabled = true;
+				break;
+			case "TextBoxRight":
+			case "TextBoxLeft":
+			case "Dropdown":
+			default:
+				$("#_width select")[0].selectedIndex = 0;
+				$("#_width select")[0].disabled = false;
+				break;
+		}
+	},
+	value: function(type,uId){		
+		document.getElementById('_value').innerHTML = "";
+		switch(type){
+			case "Paragraph":
+				setString("_valueName","Text");
 				break;
 			case "RadioButton":
 			case "Dropdown":
 			case "CheckBox":
-				setString("_valueName","click to add more checboxes:");
-				button = "<button onclick=\"addValTb('"+uId+"',null,true)\" class='pull-right btn btn-xs btn-success'><i class='glyphicon glyphicon-plus-sign'></i></button>";
-				checkVal(uId);
-				return button;
+				setString("_valueName","Value <br/><small>click to add more checboxes</small>");
+				button = "<button onclick=\"createValTb('"+uId+"',undefined)\" class='pull-right btn btn-xs btn-success'><i class='glyphicon glyphicon-plus-sign'></i></button>";
+				setString("_value",button);
+				break;
+			case "TextBoxRight":
+			case "TextBoxLeft":
+			case "TextArea":
+				setString("_valueName","Default Value:<br/><small>not required</small>");
 				break;
 			default:
 				break;
 		}
+		checkVal(uId);
 	}
+}
+
+// MODAL (ABOUT-TO) HIDE EVENT
+// $('#modal-properties').on('hide.bs.modal', function () {
+//   if (ikjj>1) {$('#modal-properties').modal('show')};
+//   ikjj++;
+//   console.log("Modal Closed");
+// })
+
+checkVal = function(uId){
+	var cP_Obj = cP[uId];
+
+	if (cP_Obj.values.length>=1) {
+		for (var i = 0; i < cP_Obj.values.length; i++) {
+			createValTb(uId,cP_Obj.values[i]);
+		};		
+	}
+	else
+		createValTb(uId,undefined);
+		 /* iterate through array or object */
+}
+
+createValTb = function(uId,value){
+	var temp = document.getElementById('_value');
+	var valTb = document.createElement("input");
+
+	valTb.className = "form-control";
+	valTb.type = "text";
+	valTb.name = uId+'-val';
+	valTb.placeholder = "insert value here";
+	valTb.value = (value==undefined) ? null : value;
+
+	temp.appendChild(valTb);
 }
 
 deleteControl = function(uId){
 
 	child   = document.getElementById(uId);
 	parent  = getParentNode(child,false);
-	console.log(child);
 
-	console.log(child);
-	console.log(parent);
 	parent.removeChild(child);
 
 	$("#modal-properties").modal("hide");
@@ -360,23 +426,90 @@ deleteControl = function(uId){
 
 }
 
+validate = function(uId){
+	if (cP[uId].type == "Paragraph") {
+		var valArr = document.getElementsByName(uId+"-val");
+		console.log(valArr[0]);
+		if(valArr[0].value.length == 0){
+			alert("Error: You Must input some value for Paragraph");
+			return;
+		}
+	}
+	else
+		$("#modal-properties").modal("hide");
+}
+
 saveControl = function(uId){
 
 	$("#modal-properties").modal("show");
-	// console.log(controlProperties[uId]);
+	var tmep = null;
+	var pObj = cP[uId];
 
-	var pObj = controlProperties[uId];
+	temp = document.querySelector("#_required input");
+	pObj.required 		= (temp.checked) ? true : false;
 
-	pObj.required 		= ($("#_required input")[0].checked) ? true : false;
-	pObj.dataType 		= $("#_dataType select")[0].selectedOptions[0].value;
-	pObj.label 			= $("#_label input")[0].value;
-	pObj.placeholder 	= $("#_placeholder input")[0].value;
-	pObj.width 			= $("#_width select")[0].selectedOptions[0].value;
+	temp = document.querySelector("#_dataType select");
+	pObj.dataType 		= temp.value;
 
-//	$("#_value").html(analyzeProperties.value(pObj.type,pObj.values.length,pObj._uid));
+	temp = document.querySelector("#_label input");
+	pObj.label 			= temp.value;
 
-	// console.log(pObj);
+	temp = document.querySelector("#_placeholder input");
+	pObj.placeholder 	= temp.value;
 
+	temp = document.querySelector("#_width select");
+	pObj.width 			= $("#_width select")[0].value;
+	// Saves Value(s)
+	var valArr = document.getElementsByName(uId+"-val");
+	for (var i = 0; i < valArr.length; i++) {
+		pObj.values[i] = valArr[i].value;
+	};
+
+	validate(uId);
+	updateControl(pObj._uid);
+}
+
+updateControl = function(uId){
+	var control = document.getElementById(uId);
+	var pObj = cP[uId];
+	var width = 0;
+
+	// set label
+	$("#"+uId+" label").html(pObj.label);
+
+	// set Placeholder
+	$("#"+uId+" input").attr('placeholder', pObj.placeholder);
+
+	// set width
+	for (var i = 0; i < control.classList.length; i++) {
+		if(control.classList[i].substring(0,7) == "col-xs-"){
+			width = control.classList[i].substring(7);
+			console.log("width: " + width);
+		}
+	}
+
+	if (pObj.width != width) {
+		control.classList.remove("col-xs-"+width);
+		control.classList.add("col-xs-"+pObj.width);
+	}
+
+	// set Values
+	var innerNode = control.querySelector("input");
+	switch(pObj.type){
+		case "Paragraph":
+			setString(control.id+" p",pObj.values[0]);
+			break;
+		case "Dropdown":
+			control.querySelector("select").options[0].text = pObj.values[0];
+		case "RadioButton":
+		case "CheckBox":
+		case "TextBoxRight":
+		case "TextBoxLeft":
+		case "TextArea":
+			break;
+		default:
+			break;
+	}
 
 }
 
@@ -384,60 +517,51 @@ resetModal = function(){
 
 }
 
-checkVal = function(uId){
-	var dataObj = controlProperties[uId];
-	var valHtml = null;
-	console.log(dataObj.values.length);
+tbCheck = 0;
+insertSeprator = function(type){ // Fetches The Last Row to input 
 
-	for (var i = 0; i < dataObj.values.length; i++) {
-		addValTb(uId,dataObj.values[i].value,false);
-	};
-		 /* iterate through array or object */
-}
+	var formObj = document.getElementById('pprlsForm');
+	var seprator = document.createElement('div');
+	seprator.className = "clearfix";
 
-addValTb = function(uId,value,appendVal){
-	valTb = document.createElement("input");
-
-	valTb.setAttribute("class", "form-control");
-	valTb.setAttribute("type", "text");
-	valTb.setAttribute("name", uId);
-	valTb.setAttribute("placeholder", "Text for "+uId);
-	valTb.setAttribute("value", value);
-	if (appendVal)
-		$("#_value").append(valTb);
-	else
-		$("#_value").html(valTb);
-}
-
-fetchRow = function(formObj){ // Fetches The Last Row to input 
-
-	if(formObj[0].childElementCount>0){
-		var rowObj = formObj.children()[formObj[0].childElementCount-1]
-
-		pprlsrowCount = formObj[0].childElementCount; //Double Check Row Count In Case Deleted
-		
-		if (isRowFull(rowObj))
-			return rowObj;
-		else
-			return createRow(formObj);
-		
+	switch(type){
+		case "TextBoxLeft":
+		case "TextBoxRight":
+		case "Dropdown":
+			tbCheck++;
+			if (tbCheck>2) {
+				console.log(tbCheck);
+				formObj.appendChild(seprator);
+				tbCheck = 0;
+			}
+			return 'pprlsForm';
+			break;
+		case "TextArea":
+		default:
+			formObj.appendChild(seprator);
+			tbCheck = 2;
+			return 'pprlsForm';
+			break;
 	}
-	else if (formObj[0].childElementCount==0)
-		return createRow(formObj);	
 }
 
 isRowFull = function(rowObj){ // Checks if the row has more the 2 elements [requires row (jquery) obj]
-	if($(rowObj)[0].childElementCount>=2)
-		return false;
-	return true;
+	if(rowObj.childElementCount>=2)
+		return true;
+	return false;
 }
 
-createRow = function(formObj){ // If the row is not available or is full Create Row function is called
+createRow = function(){ // If the row is not available or is full Create Row function is called
+	var formObj = document.getElementById('pprlsForm');
 	pprlsrowCount++;
-	var rowHTML = "<div id='pprls_Row"+pprlsrowCount+"'></div>";
+	var rowHTML = document.createElement('div');
+	rowHTML.id = 'pprls_Row'+pprlsrowCount;
 
-	formObj.append(rowHTML);
-	return fetchRow(formObj);
+	formObj.appendChild(rowHTML);
+
+	var rowObj = formObj.children[formObj.childElementCount];
+
+	return rowHTML.id;
 }
 
 generateId = function(){
@@ -446,7 +570,6 @@ generateId = function(){
 
 	for( var i=0; i < 3; i++ )
 		genItrator += possible.charAt(Math.floor(Math.random() * possible.length));
-	// console.log("Generated: "+genItrator);
 	return genItrator;
 }
 
@@ -470,3 +593,6 @@ setString = function(id,string){
 }
 
 window.onerror = function(ex) { alert(ex) };
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
